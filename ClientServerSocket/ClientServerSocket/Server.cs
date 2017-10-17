2f5f3a96e7a02b98c745510b1798221ce1586f8b
Khyteang Lim <khy_lim@student.uml.edu>
2017-10-17 10:19:44 -0400

    first commit

---
+++ ClientServerSocket/ClientServerSocket/Server.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ClientServerSocket
+{
+	class Server
+	{
+		private static byte[] _buffer = new byte[1024];
+		private static List<Socket> _clientSocket = new List<Socket>();
+		private static Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		private static int SERVER_PORT = 0;
+
+		static void Main(string[] args)
+		{
+			var serverPort = Convert.ToInt32(args[SERVER_PORT]);
+			SetupServer(serverPort);
+			Console.ReadLine();
+		}
+
+		private static void SetupServer(int port)
+		{
+			Console.WriteLine("Setting up server...");
+			_serverSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+			_serverSocket.Listen(10);
+			_serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+
+		}
+
+		private static void AcceptCallback(IAsyncResult ar)
+		{
+			Socket socket = _serverSocket.EndAccept(ar);
+			_clientSocket.Add(socket);
+			Console.WriteLine("Client connects...");
+			socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+			_serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+
+		}
+
+		private static void ReceiveCallback(IAsyncResult ar)
+		{
+			Socket socket = (Socket)ar.AsyncState;
+			int received = socket.EndReceive(ar);
+			byte[] dataBuf = new byte[received];
+			Array.Copy(_buffer, dataBuf, received);
+
+			string text = Encoding.ASCII.GetString(dataBuf);
+			Console.WriteLine("Text received: " + text);
+
+			var req = text.Split(',');
+			var method = req[0];
+			var filename = req[1];
+			var path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+			if (method.ToUpper() == "GET")
+			{
+				string textFromFile = File.ReadAllText(path + "/" + filename);
+				byte[] result = Encoding.ASCII.GetBytes(textFromFile);
+				socket.BeginSend(result, 0, result.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+			}
+			else if (method.ToUpper() == "PUT")
+			{
+
+			}
+			else
+			{
+
+			}
+		}
+
+		private static void SendCallback(IAsyncResult ar)
+		{
+			Socket socket = (Socket)ar.AsyncState;
+			socket.EndSend(ar);
+		}
+	}
+}
---
+++ ClientSocket/ClientSocket/Client.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace ClientSocket
+{
+	class Client
+	{
+		private static int SERVER_IP = 0;
+		private static int SERVER_PORT = 1;
+		private static int METHOD = 2;
+		private static int FILENAME = 3;
+
+		private static Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		static void Main(string[] args)
+		{
+			var serverIP = args[SERVER_IP];
+			var serverPort = Convert.ToInt32(args[SERVER_PORT]);
+			var method = args[METHOD];
+			var filename = args[FILENAME];
+
+			Console.Title = "Client";
+			ConnectServer(serverIP, serverPort);
+			SendRequest(serverIP, method.ToUpper(), filename);
+			Console.ReadLine();
+		}
+
+		private static void SendRequest(string serverIP, string method, string filename)
+		{
+			//"127.0.0.1"
+			var request = $"{method} / HTTP/1.1\r\n" +
+				$"Host: {serverIP}\r\n" +
+				"Connection: keep-alive\r\n" +
+				"Accept: text/html\r\n" +
+				"User-Agent: CSharpTests\r\n\r\n";
+			//var request = $"{method},{filename}";
+			_clientSocket.Send(Encoding.ASCII.GetBytes(request));
+
+			byte[] receivedBuf = new byte[50240];
+			var blob = _clientSocket.Receive(receivedBuf);
+			string text = Encoding.ASCII.GetString(receivedBuf);
+			var path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+			Console.WriteLine(text);
+			File.WriteAllText(path+'/'+filename, text);
+			/*
+			while(true)
+			{
+				_clientSocket.RemoteEndPoint
+			}
+			*/
+		}
+
+		private static void ConnectServer(string serverIP, int port)
+		{
+			while (!_clientSocket.Connected)
+			{
+				try
+				{
+					_clientSocket.Connect(serverIP, port);
+				}
+				catch (SocketException)
+				{
+					Console.WriteLine("Retrying to connect to server...");
+				}
+			}
+			Console.Clear();
+			Console.WriteLine("Client Connected...");
+
+		}
+	}
+}
